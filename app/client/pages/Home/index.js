6ff3e2f16c7a92643b675e17cbdf64dab39ef326
Yaphet Ye <tsyeyuanfeng@126.com>
2015-11-18 17:57:02 +0800

    部署版本

--- app/client/pages/Home/index.js
+++ app/client/pages/Home/index.js
@@ -4,13 +4,26 @@ import RoundButton from '../../components/RoundButton';
 import './styles/index.less';
 
 export default class index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isSigned: window.localStorage.getItem('isSigned')
+        };
+    }
+
+    signup() {
+        window.localStorage.setItem('isSigned', true);
+        this.setState({isSigned: true});
+    }
+
     render() {
+
         return (
             <div className="home-index-page">
-                <RoundButton link={true} href="#" className="btn-sign" lineHeight="1.8rem">
+                <RoundButton className="btn-sign" lineHeight="1.8rem" onClick={this.signup.bind(this)}>
                     <Icon name="pencil"/>
                     <br/>
-                    签到
+                    {this.state.isSigned ? '已签到' : '签到'}
                 </RoundButton>
             </div>
         );
