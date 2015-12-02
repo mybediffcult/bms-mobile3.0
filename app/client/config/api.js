f098dc89ebe0980ae4579c19c261cdf01e7ae481
Yaphet Ye <tsyeyuanfeng@126.com>
2015-12-02 10:21:12 +0800

    修改委托审核

--- app/client/config/api.js
+++ app/client/config/api.js
@@ -1,4 +1,4 @@
 export default {
-    prefix: 'http://106.38.138.61:3000/api/',
-    bms_prefix: 'http://106.38.138.61:8088/bms/public/index.php?controller=api&'
+    prefix: 'http://106.38.138.99:3000/api/',
+    bms_prefix: 'http://106.38.138.99:8080/bms/public/index.php?controller=api&'
 };
