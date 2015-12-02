fdb343d65594021d32635c11013cc97db7f76335
Yaphet Ye <tsyeyuanfeng@126.com>
2015-12-02 15:51:40 +0800

    修改节目单列表

--- app/client/pages/Program/list.js
+++ app/client/pages/Program/list.js
@@ -66,10 +66,10 @@ export default class list extends React.Component {
                     设备节目单
                 </h2>
                 <p className="subtitle">{JSON.parse(window.localStorage.getItem('administration')).administrationName}</p>
-                {this.state.loading ? '' : this.getPrograms(this.state.programList.countryProgram, '国家节目')}
-                {this.state.loading ? '' : this.getPrograms(this.state.programList.provinceProgram, '省节目')}
-                {this.state.loading ? '' : this.getPrograms(this.state.programList.cityProgram, '市节目')}
-                {this.state.loading ? '' : this.getPrograms(this.state.programList.terminalProgram, '设备自有节目')}
+                {this.state.loading ? '' : this.getPrograms(this.state.programList.countryProgram, '卫计委时段')}
+                {this.state.loading ? '' : this.getPrograms(this.state.programList.provinceProgram, '省时段')}
+                {this.state.loading ? '' : this.getPrograms(this.state.programList.cityProgram, '市时段')}
+                {this.state.loading ? '' : this.getPrograms(this.state.programList.terminalProgram, '自选时段')}
                 <RaisedButton style={{width: '90%', margin: '1rem 5%'}} label="推送节目单" secondary={true} onClick={this.push.bind(this)} />
             </div>
         );
