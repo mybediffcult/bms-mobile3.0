e4cc64c3af429afd323d86dfc332f2b0152b5dfb
Yaphet Ye <tsyeyuanfeng@126.com>
2015-12-27 09:58:14 +0800

    debug

--- app/client/pages/Program/list.js
+++ app/client/pages/Program/list.js
@@ -40,6 +40,166 @@ export default class list extends React.Component {
         };
     }
 
+    componentWillMount() {
+        this.unsubscribeUserStore = UserStore.listen(this.onUserStoreChange.bind(this));
+        this.unsubscribeProgramStore = ProgramStore.listen(this.onProgramStoreChange.bind(this));
+        this.unsubscribeTerminalStore = TerminalStore.listen(this.onTerminalStoreChange.bind(this));
+        UserActions.getUser();
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeUserStore();
+        this.unsubscribeProgramStore();
+        this.unsubscribeTerminalStore();
+    }
+
+    /**
+     * 监听用户数据变化
+     * @param data
+     */
+    onUserStoreChange(data) {
+        this.setState({administration: data}, function() {
+            TerminalActions.fetchAll(this.state.administration.administrationid);
+        });
+    }
+
+    /**
+     * 监听设备数据变化
+     * @param data
+     */
+    onTerminalStoreChange(data) {
+        if(data instanceof Array) {
+            if(!this.state.terminalId && data.length > 0)
+                this.setState({terminalList: data, terminalId: data[0].terminalid}, function() {
+                    TerminalActions.getDateWithProgram(this.state.terminalId);
+                    ProgramActions.fetch(this.state.terminalId, this.state.date);
+                });
+            else
+                this.setState({terminalList: data}, function() {
+                    TerminalActions.getDateWithProgram(this.state.terminalId);
+                    ProgramActions.fetch(this.state.terminalId, this.state.date);
+                });
+        }
+        else if(data){
+            if(data.startdate) {
+                this.setState({startDate: moment(data.startdate, "YYYYMMDD").toDate()});
+            }
+
+            if(data.enddate) {
+                this.setState({endDate: moment(data.enddate, "YYYYMMDD").toDate()});
+            }
+
+        }
+    }
+
+    /**
+     * 监听节目单数据变化
+     * @param data
+     */
+    onProgramStoreChange(data) {
+        this.setState({programList: data});
+    }
+
+
+    /**
+     * 获取节目单
+     * @param programs
+     * @returns {*}
+     */
+    getPrograms(programs) {
+        if(programs.length > 0) {
+            return programs.map((program, index)=>{
+
+                    var timebuckets = program.timebucket.split("-");
+                    var startTime   = moment(timebuckets[0], "HHmm").format("HH:mm");
+                    var endTime     = moment(timebuckets[1], "HHmm").format("HH:mm");
+
+                    var contentList = program.sequence.map((content, index)=>{
+                        return (
+                            <li key={"material-" + index}>
+                                <div className="left" style={{backgroundImage: 'url(' + 'http://106.38.138.99:8080/bms/public/' + content.imagepath + ')'}}>
+                                </div>
+                                <div className="right">
+                                    <p className="title">{content.contenttitle}</p>
+                                    <p className="duration"><Icon name="clock-o"/> {Math.ceil(content.length / 60)}分钟</p>
+                                </div>
+                            </li>
+                        );
+                    });
+
+                    return (
+                        <li key={"program-" + index}>
+                            <div className="time">{startTime}</div>
+                            <ul className="material-list">
+                                {contentList}
+                            </ul>
+                        </li>
+                    )}
+            );
+        }
+        else {
+            return null;
+        }
+    }
+
+
+    /**
+     * 日期选择触发
+     */
+    toggleDayPicker() {
+        this.setState({isDatePickerOpen: !this.state.isDatePickerOpen});
+    }
+
+    /**
+     * 日期选择回调
+     * @param e
+     * @param day
+     * @param modifiers
+     */
+    onDayPick(e, day, modifiers) {
+        if(modifiers == "disabled") {
+            return;
+        }
+        this.setState({date: day});
+        this.toggleDayPicker();
+        this.setState({programList: []});
+        ProgramActions.fetch(this.state.terminalId, this.state.date);
+    }
+
+    /**
+     * 后一天
+     */
+    onNextDay() {
+        var date = this.state.date;
+        this.setState({date: moment(date).add(1, 'day').toDate()});
+        this.setState({programList: []});
+        ProgramActions.fetch(this.state.terminalId, this.state.date);
+    }
+
+    /**
+     * 前一天
+     */
+    onPrevDay() {
+        var date = this.state.date;
+        this.setState({date: moment(date).subtract(1, 'day').toDate()});
+        this.setState({programList: []});
+        ProgramActions.fetch(this.state.terminalId, this.state.date);
+    }
+
+    /**
+     * 设备选择触发
+     */
+    toggleTerminalPicker() {
+        this.setState({isTerminalPickerOpen: !this.state.isTerminalPickerOpen});
+    }
+
+    /**
+     * 设备选择回调
+     * @param terminalid
+     */
+    onTerminalPick(terminalid) {
+        this.setState({isTerminalPickerOpen: false, terminalId: terminalid});
+    }
 
     render() {
 
@@ -50,7 +210,8 @@ export default class list extends React.Component {
                     mainText="设备"
                     mainIcon={<Icon name="angle-down" />}
                     rightText="创建"
-                    onRightClick={()=>{window.location.href = "#/program/edit"}}/>
+                    onRightClick={()=>{window.location.href = "#/program/edit"}}
+                    onMainClick={this.toggleTerminalPicker.bind(this)} />
 
 
                 <Navigation/>
