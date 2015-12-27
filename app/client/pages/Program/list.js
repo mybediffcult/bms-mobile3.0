c1f86594e1c2100c52328c544cba813bc8ec3856
Yaphet Ye <tsyeyuanfeng@126.com>
2015-12-27 10:10:38 +0800

    debug

--- app/client/pages/Program/list.js
+++ app/client/pages/Program/list.js
@@ -202,16 +202,22 @@ export default class list extends React.Component {
     }
 
     render() {
+        var self = this;
+        var modifiers = {
+            selected: function(day) {
+                return DateUtils.isSameDay(day, self.state.date);
+            },
+            disabled: function(day) {
+                return day > self.state.endDate || day < self.state.startDate;
+            }
+        };
 
 
-        var terminal = this.state.terminalList.find((terminal)=>{
-            return terminal.terminalid == this.state.terminalId;
-        });
 
         return (
             <div className="program-list-page">
                 <NavBar
-                    mainText={(terminal && terminal.name) ? terminal.name : ''}
+                    mainText={'设备'}
                     mainIcon={<Icon name="angle-down" />}
                     rightText="创建"
                     onRightClick={()=>{window.location.href = "#/program/edit"}}
