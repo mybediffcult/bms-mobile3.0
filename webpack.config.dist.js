ddf6e79fe5d9693e9d37982dfefb3e63431c0659
Yaphet Ye <tsyeyuanfeng@126.com>
2015-12-27 09:19:13 +0800

    debug

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -40,10 +40,10 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
+        /*new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
-        })
+        })*/
     ]
 };
